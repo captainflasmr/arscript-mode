797e1d0ef1312e8ff846abd0c6853358041f7691
james@dyerdwelling.family <james@dyerdwelling.family>
2024-08-19 20:27:33 +0100

    Add GPL-3.0 LICENSE and updated arscript-mode.el with proper indent
    - Added the GPL-3.0 license to the project.
    - Reset indent with `(setq-default lisp-indent-offset nil)` to the Emacs default, rather than the value of 2 which I had set for some reason!!

--- arscript-mode.el
+++ arscript-mode.el
@@ -37,29 +37,29 @@
   (interactive)
   (beginning-of-line)
   (if (bobp)  ; Check if it's the beginning of the buffer
-    (indent-line-to 0)  ; Set indent to 0 at the start of the buffer.
+      (indent-line-to 0)  ; Set indent to 0 at the start of the buffer.
     (let ((not-indented t) (cur-indent nil))
       (if (looking-at "^[ \t]*\\(</\\)")
-        (progn
-          (save-excursion
-            (forward-line -1)
-            (setq cur-indent (- (current-indentation) tab-width)))
-          (when (< cur-indent 0)
-            (setq cur-indent 0)))
+          (progn
+            (save-excursion
+              (forward-line -1)
+              (setq cur-indent (- (current-indentation) tab-width)))
+            (when (< cur-indent 0)
+              (setq cur-indent 0)))
         (save-excursion
           (while not-indented
             (forward-line -1)
             (cond
-              ;; If the line starts with a closing tag, match the opening tag's indentation.
-              ((looking-at "^.*[ \t]*</") ; Matches a closing tag
-                (setq cur-indent (current-indentation))
-                (setq not-indented nil))
-              ;; If the line starts with an opening tag, indent the next line more.
-              ((looking-at "^.*[ \t]*<[^/]")
-                (setq cur-indent (+ (current-indentation) tab-width))
-                (setq not-indented nil))
-              ((bobp)
-                (setq not-indented nil))))))
+             ;; If the line starts with a closing tag, match the opening tag's indentation.
+             ((looking-at "^.*[ \t]*</") ; Matches a closing tag
+              (setq cur-indent (current-indentation))
+              (setq not-indented nil))
+             ;; If the line starts with an opening tag, indent the next line more.
+             ((looking-at "^.*[ \t]*<[^/]")
+              (setq cur-indent (+ (current-indentation) tab-width))
+              (setq not-indented nil))
+             ((bobp)
+              (setq not-indented nil))))))
       (indent-line-to (or cur-indent (current-indentation) 0)))))
 
 ;;;###autoload
@@ -68,73 +68,73 @@
   (kill-all-local-variables)
   ;; Syntax highlighting
   (setq font-lock-defaults
-    `((
-        ;; Comments
-        ("\\(//.*\\)" . font-lock-comment-face)
+        `((
+           ;; Comments
+           ("\\(//.*\\)" . font-lock-comment-face)
 
-        ;; Keywords
-        (,(regexp-opt '("EvType" "CommandID" "ParamType" "Value" "Count"
-                         "Idx" "Channels" "Path"
-                         "Script Startup Features"
-                         "Reference Image"
-                         "ArtRage Version" "ArtRage Build"
-                         "Professional Edition" "Script Version"
-                         "Painting Name" "Painting Width" "Painting Height"
-                         "Painting DPI" "Mask Edge Map Width"
-                         "Mask Edge Map Height" "Mask Edge Map Height"
-                         "Author Name" "Script Name" "Comment"
-                         "Script Type" "Script Feature Flags"
-                         "Tool Data")
-            'words)
-          . font-lock-keyword-face)
+           ;; Keywords
+           (,(regexp-opt '("EvType" "CommandID" "ParamType" "Value" "Count"
+                           "Idx" "Channels" "Path"
+                           "Script Startup Features"
+                           "Reference Image"
+                           "ArtRage Version" "ArtRage Build"
+                           "Professional Edition" "Script Version"
+                           "Painting Name" "Painting Width" "Painting Height"
+                           "Painting DPI" "Mask Edge Map Width"
+                           "Mask Edge Map Height" "Mask Edge Map Height"
+                           "Author Name" "Script Name" "Comment"
+                           "Script Type" "Script Feature Flags"
+                           "Tool Data")
+                         'words)
+            . font-lock-keyword-face)
 
-        ;; constants
-        (,(regexp-opt '("SetForeColour" "SetForeColor" "Pixel" "Command" "Yes" "No"
-                         "CanvasXForm" "ReferenceImageXForm" "SetMetallicValue"
-                         "Undo" "SetToolProperty" "ToolProp" "CID_SetClearCanvas"
-                         "CID_ToolSelect" "CID_SetSpecificToolPressure"
-                         "CID_ToggleSpecificLayerVisible" "CID_MergeSpecificLayerDown"
-                         "CID_DuplicateSpecificLayer" "CID_SetSpecificLayerBlend"
-                         "LayerXForm" "CID_SelectSpecificLayer"
-                         "CID_SetSpecificLayerOpacity" "LayerProp"
-                         "ExportLayer" "Canvas Reset All"
-                         "LoadReferenceImage" "ToolPreset" "flag" "true")
-            'words)
-          . font-lock-constant-face)
+           ;; constants
+           (,(regexp-opt '("SetForeColour" "SetForeColor" "Pixel" "Command" "Yes" "No"
+                           "CanvasXForm" "ReferenceImageXForm" "SetMetallicValue"
+                           "Undo" "SetToolProperty" "ToolProp" "CID_SetClearCanvas"
+                           "CID_ToolSelect" "CID_SetSpecificToolPressure"
+                           "CID_ToggleSpecificLayerVisible" "CID_MergeSpecificLayerDown"
+                           "CID_DuplicateSpecificLayer" "CID_SetSpecificLayerBlend"
+                           "LayerXForm" "CID_SelectSpecificLayer"
+                           "CID_SetSpecificLayerOpacity" "LayerProp"
+                           "ExportLayer" "Canvas Reset All"
+                           "LoadReferenceImage" "ToolPreset" "flag" "true")
+                         'words)
+            . font-lock-constant-face)
 
-        ;; brush types
-        (,(regexp-opt '("Oil Brush" "Eraser"
-                         "Oil Paint")
-            'words)
-          . font-lock-string-face)
+           ;; brush types
+           (,(regexp-opt '("Oil Brush" "Eraser"
+                           "Oil Paint")
+                         'words)
+            . font-lock-string-face)
 
-        ;; Tags like <Version>, <Header>
-        ("\\(<[^>]+>\\)" . font-lock-type-face)
+           ;; Tags like <Version>, <Header>
+           ("\\(<[^>]+>\\)" . font-lock-type-face)
 
-        ;; Numerical pairs of values (<num>, <num>)
-        ("\\(\\<\\(?:Loc\\|Dr\\|Hd\\|Off\\|Size\\|Scale\\):\\)\\s-+\\((\\)\\([-0-9\\.]+\\),\\s-*\\([-0-9\\.]+\\)\\()\\)"
-         (1 font-lock-keyword-face)
-         (3 font-lock-constant-face)
-         (4 font-lock-constant-face))
+           ;; Numerical pairs of values (<num>, <num>)
+           ("\\(\\<\\(?:Loc\\|Dr\\|Hd\\|Off\\|Size\\|Scale\\):\\)\\s-+\\((\\)\\([-0-9\\.]+\\),\\s-*\\([-0-9\\.]+\\)\\()\\)"
+            (1 font-lock-keyword-face)
+            (3 font-lock-constant-face)
+            (4 font-lock-constant-face))
 
-        ;; hex values
-        ("0x[0-9A-Fa-f]+"
-          . font-lock-string-face)
+           ;; hex values
+           ("0x[0-9A-Fa-f]+"
+            . font-lock-string-face)
 
-        ;;
-        ("\\(\\<\\(?:Rv\\|Iv\\):\\)\\s-+\\(.*\\)"
-          (1 font-lock-string-face)
-          (2 font-lock-constant-face))
+           ;;
+           ("\\(\\<\\(?:Rv\\|Iv\\):\\)\\s-+\\(.*\\)"
+            (1 font-lock-string-face)
+            (2 font-lock-constant-face))
 
-        ;; Separate out Wait for variation
-        ("\\(Wait:\\)\s+\\([0-9\\.s]+\\)"
-          (1 font-lock-string-face)
-          (2 font-lock-constant-face))
+           ;; Separate out Wait for variation
+           ("\\(Wait:\\)\s+\\([0-9\\.s]+\\)"
+            (1 font-lock-string-face)
+            (2 font-lock-constant-face))
 
-        ;; Single numerical value
-        ("\\([A-Za-z]+:\\)\s+\\([0-9\\.s]+\\)"
-          (1 font-lock-keyword-face)
-          (2 font-lock-constant-face)))))
+           ;; Single numerical value
+           ("\\([A-Za-z]+:\\)\s+\\([0-9\\.s]+\\)"
+            (1 font-lock-keyword-face)
+            (2 font-lock-constant-face)))))
   ;; Comments
   (setq comment-start "//")
   (setq comment-end "")
